49ab9b371993a1b79baeda5d4cfbab6a21b2fdc6
eric ou <ouchunxiao@gmail.com>
2014-11-06 11:16:02 +0800

    add  bootstrap modal and notify

--- docs/admin_examples/index/form.js
+++ docs/admin_examples/index/form.js
@@ -1,1 +1,23 @@
 //do something
+var Form = (function($) {
+    return {
+        init: function() {
+            $('[data-toggle="popBreadcrumb"]').on('pop.mu.breadcrumb', function(e) {
+
+                MessageBox.info('系统发生未知异常消息!', '异常信息');
+
+            });
+        },
+        processFormSave: function(dtd) {　　
+            var tasks = function() {
+                dtd.resolve();
+            };
+
+            setTimeout(tasks, 5000);
+        }
+    };
+}(jQuery));
+
+jQuery(function() {
+    Form.init();
+});
