3f4b84691df0547e07c567acf0c9832acf4df671
eric ou <ouchunxiao@gmail.com>
2015-02-03 14:39:08 +0800

    add beyond index

--- docs/examples/admin/index/index.js
+++ docs/examples/admin/index/index.js
@@ -42,8 +42,89 @@ var Index = (function($) {
     };
 }(jQuery));
 
+InitiateSideMenu();
+
+function InitiateSideMenu() {
+
+    //Sidebar Toggler
+    $(".sidebar-toggler").on('click', function () {
+        $("#sidebar").toggleClass("hide");
+        $(".sidebar-toggler").toggleClass("active");
+        return false;
+    });
+    //End Sidebar Toggler
+
+    //Sidebar Collapse
+    var b = $("#sidebar").hasClass("menu-compact");
+    $("#sidebar-collapse").on('click', function () {
+        if (!$('#sidebar').is(':visible'))
+            $("#sidebar").toggleClass("hide");
+        $("#sidebar").toggleClass("menu-compact");
+        $(".sidebar-collapse").toggleClass("active");
+        b = $("#sidebar").hasClass("menu-compact");
+
+        if ($(".sidebar-menu").closest("div").hasClass("slimScrollDiv")) {
+            $(".sidebar-menu").slimScroll({ destroy: true });
+            $(".sidebar-menu").attr('style', '');
+        }
+        if (b) {
+            $(".open > .submenu")
+                .removeClass("open");
+        } else {
+            if ($('.page-sidebar').hasClass('sidebar-fixed')) {
+                var position = (readCookie("rtl-support") || location.pathname == "/index-rtl-fa.html" || location.pathname == "/index-rtl-ar.html") ? 'right' : 'left';
+                $('.sidebar-menu').slimscroll({
+                    height: 'auto',
+                    position: position,
+                    size: '3px',
+                    color: themeprimary
+                });
+            }
+        }
+        //Slim Scroll Handle
+
+
+
+    });
+    //End Sidebar Collapse
+
+
+    //Sidebar Menu Handle
+    $(".sidebar-menu").on('click', function (e) {
+        var menuLink = $(e.target).closest("a");
+        if (!menuLink || menuLink.length == 0)
+            return;
+        if (!menuLink.hasClass("menu-dropdown")) {
+            if (b && menuLink.get(0).parentNode.parentNode == this) {
+                var menuText = menuLink.find(".menu-text").get(0);
+                if (e.target != menuText && !$.contains(menuText, e.target)) {
+                    return false;
+                }
+            }
+            return;
+        }
+        var submenu = menuLink.next().get(0);
+        if (!$(submenu).is(":visible")) {
+            var c = $(submenu.parentNode).closest("ul");
+            if (b && c.hasClass("sidebar-menu"))
+                return;
+            c.find("> .open > .submenu")
+                .each(function () {
+                    if (this != submenu && !$(this.parentNode).hasClass("active"))
+                        $(this).slideUp(200).parent().removeClass("open");
+                });
+        }
+        if (b && $(submenu.parentNode.parentNode).hasClass("sidebar-menu"))
+            return false;
+        $(submenu).slideToggle(200).parent().toggleClass("open");
+        return false;
+    });
+    //End Sidebar Menu Handle
+}
+
 $(document).ready(function() {
 
     Index.init();
 
 });
+
