0672e4ed6c30c84b4be496927b2232dcaf42e256
Eric.Ou <ouchunxiao@gmail.com>
2015-09-13 17:37:55 +0800

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,9 +55,15 @@ var App = (function($, utils, window, document, undefined) {
             var isLocal = window.location.href.indexOf('macula.top');
             if(isLocal != -1){
                 $.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
-                    var isJs = options.url.indexOf(".js");
-                    if( isJs == -1)
+                    var extension = options.url.slice((options.url.lastIndexOf(".") - 1 >>> 0) + 2);
+                    if( extension !== 'js')
                       options.url = "http://macula.top/mower/"  + options.url;
+                });
+            }else{
+                $.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
+                    var extension = options.url.slice((options.url.lastIndexOf(".") - 1 >>> 0) + 2);
+                    if( extension !== 'js')
+                      options.url = "http://localhost:9000/"  + options.url;
                 });
             }
 
