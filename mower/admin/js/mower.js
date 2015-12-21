fb9ae3149d2901e9823d313d68254c434378b0b0
Eric.Ou <ouchunxiao@gmail.com>
2015-12-21 09:11:51 +0800

    Updates

--- mower/admin/js/mower.js
+++ mower/admin/js/mower.js
@@ -37015,7 +37015,7 @@ else if ( jQuery && !jQuery.fn.dataTable.select ) {
 })(jQuery, window);
 
 ;/*!
- * mower - v1.1.1 - 2015-12-11
+ * mower - v1.1.1 - 2015-12-17
  * Copyright (c) 2015 Infinitus, Inc.
  * Licensed under Apache License 2.0 (https://github.com/macula-projects/mower/blob/master/LICENSE)
  */
@@ -37038,6 +37038,104 @@ var UniqueId = (function() {
         return prefix;
     };
 })();
+
+// Production steps of ECMA-262, Edition 5, 15.4.4.14
+// Reference: http://es5.github.io/#x15.4.4.14
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function(searchElement, fromIndex) {
+
+    var k;
+
+    // 1. Let O be the result of calling ToObject passing
+    //    the this value as the argument.
+    if (this == null) {
+      throw new TypeError('"this" is null or not defined');
+    }
+
+    var O = Object(this);
+
+    // 2. Let lenValue be the result of calling the Get
+    //    internal method of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0;
+
+    // 4. If len is 0, return -1.
+    if (len === 0) {
+      return -1;
+    }
+
+    // 5. If argument fromIndex was passed let n be
+    //    ToInteger(fromIndex); else let n be 0.
+    var n = +fromIndex || 0;
+
+    if (Math.abs(n) === Infinity) {
+      n = 0;
+    }
+
+    // 6. If n >= len, return -1.
+    if (n >= len) {
+      return -1;
+    }
+
+    // 7. If n >= 0, then Let k be n.
+    // 8. Else, n<0, Let k be len - abs(n).
+    //    If k is less than 0, then let k be 0.
+    k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+    // 9. Repeat, while k < len
+    while (k < len) {
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the
+      //    HasProperty internal method of O with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      //    i.  Let elementK be the result of calling the Get
+      //        internal method of O with the argument ToString(k).
+      //   ii.  Let same be the result of applying the
+      //        Strict Equality Comparison Algorithm to
+      //        searchElement and elementK.
+      //  iii.  If same is true, return k.
+      if (k in O && O[k] === searchElement) {
+        return k;
+      }
+      k++;
+    }
+    return -1;
+  };
+}
+
+// Production steps of ECMA-262, Edition 5, 15.4.4.21
+// Reference: http://es5.github.io/#x15.4.4.21
+if (!Array.prototype.reduce) {
+  Array.prototype.reduce = function(callback /*, initialValue*/) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError('Array.prototype.reduce called on null or undefined');
+    }
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + ' is not a function');
+    }
+    var t = Object(this), len = t.length >>> 0, k = 0, value;
+    if (arguments.length == 2) {
+      value = arguments[1];
+    } else {
+      while (k < len && !(k in t)) {
+        k++; 
+      }
+      if (k >= len) {
+        throw new TypeError('Reduce of empty array with no initial value');
+      }
+      value = t[k++];
+    }
+    for (; k < len; k++) {
+      if (k in t) {
+        value = callback(value, t[k], k, t);
+      }
+    }
+    return value;
+  };
+}
 
 /**
  * 线形树，不修改原始数据，只是原始数据的重新排序.
@@ -38623,7 +38721,6 @@ var Base = (function($, utils, window, document, undefined) {
         });
     };
 
-
     var _parseComposite = function(key, value, t, prefix, parentkey) {
         var opts = {};
         var attrName = 'data-' + prefix + (parentkey ? (parentkey.toLowerCase() + '-') : '') + key.toLowerCase();
@@ -38669,7 +38766,7 @@ var Base = (function($, utils, window, document, undefined) {
             var pos = position == 'fit' ? (half * 2 / 4) : (position == 'center' ? half : position);
             $dialog.css('margin-top', pos);
         });
-    }
+    };
 
     // public functions
     base.init = function() {
@@ -38678,6 +38775,7 @@ var Base = (function($, utils, window, document, undefined) {
         _resetIconContent();
         _enterToTab();
         _ajustModalPosition('fit');
+
     };
 
     /**
@@ -45214,8 +45312,8 @@ jQuery(function() {
             var that = this;
             //add cascade items
             $.each(this.options.cascadeItems,function(index,value){
-                that.$cascadeheader.append('<li><a href="#cascade_'+ index + '"  data-toggle="tab">'+ value +'</a></li>');
-                that.$cascadecontent.append('<div class="tab-pane" id="cascade_'+ index +'"></div>');
+                that.$cascadeheader.append('<li><a href="#cascade_'+ index +'_' + that.options.name + '"  data-toggle="tab">'+ value +'</a></li>');
+                that.$cascadecontent.append('<div class="tab-pane" id="cascade_'+ index +'_' + that.options.name +'"></div>');
             });
 
             this.$cascadeheader.find('li:first').addClass('active');
